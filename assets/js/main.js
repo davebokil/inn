a1751d9e8e6c374abd99d0dc7170af5711b72988
Dave Bokil <bokild@gmail.com>
2017-12-15 17:02:29 -0500

    formspree

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+var contactform = document.getElementById('contact');
+// contactform.setAttribute('action', '//formspree.io/' + 'mtr72' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
+contactform.setAttribute('action', '//formspree.io/' + 'bokild' + '@' + 'gmail' + '.' + 'com');
