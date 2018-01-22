384f1501a6f2c9befa8ee4a2d5693c9e2783507d
Dave Bokil <bokild@gmail.com>
2018-01-22 16:12:46 -0500

    cc two

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
-// var contactform = document.getElementById('contact');
-// contactform.setAttribute('action', '//formspree.io/' + 'mtr72' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
+// var cc = document.getElementById('cc');
+// cc.setAttribute('value', 'dpagnozzi' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
 
-var ccc = document.getElementById('cc');
+// var cctwo = document.getElementById('cctwo');
+// cctwo.setAttribute('value', 'binstein' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
+
+var cc = document.getElementById('cc');
 cc.setAttribute('value', 'bokild' + '@' + 'gmail' + '.' + 'com');
 
-
-
-
+var cct = document.getElementById('cct');
+cct.setAttribute('value', 'david_bokil' + '@' + 'bloomfield' + '.' + 'edu');
+console.log(cct)
