e821b692c6e7171088b43c67159ca2eaeae7a070
Dave Bokil <bokild@gmail.com>
2018-03-09 11:26:00 -0500

    form final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
-// var cc = document.getElementById('cc');
-// cc.setAttribute('value', 'dpagnozzi' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
-
-// var cct = document.getElementById('cct');
-// cct.setAttribute('value', 'binstein' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
-
 var cc = document.getElementById('cc');
-cc.setAttribute('value', 'djb405' + '@' + 'nyu' + '.' + 'edu');
+cc.setAttribute('value', 'dpagnozzi' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
 
 var cct = document.getElementById('cct');
-cct.setAttribute('value', 'david' + '_' + 'bokil' + '@' + 'bloomfield' + '.' + 'edu');
+cct.setAttribute('value', 'binstein' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
